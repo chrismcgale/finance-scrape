51fc388a051b799260b3c3afcfb61ebcd539b8c0
cwmcgale <cwmcgale@linux.student.cs.uwaterloo.ca>
2021-03-13 14:16:56 -0500

    Big changes to app

--- app.js
+++ app.js
@@ -11,15 +11,11 @@ puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox', '--window-size=1920,1080']
 }).then(async browser => {
 
-
     // create new page
     const page = await browser.newPage();
 
     // open file stream
     const fs = require('fs');
-
-    const TOlong = -79.383186
-    const TOlat = 43.653225
 
     var csvCols = [];
 
@@ -38,274 +34,110 @@ puppeteer.launch({
             throw new PageContactError("Ticker ".concat(tick).concat(" dose not exist"));
         }
 
-        // use the string in the split function as a delimeter for the url 
-        var split = urls[i].split("https://www.zolo.ca/toronto-real-estate/");
-        // if the url is a sold house
-        if (split[1] != null) {
-            var firstChar = split[1][0];
-            // if link is a valid address...
-            if (!used_urls.has(urls[i]) && firstChar >= '0' && firstChar <= '9') {
-                console.log(urls[i]);
-                console.log(i)
-                used_urls.add(urls[i]);
 
-                try {
-                    await page.goto(urls[i], { waitUntil: 'networkidle2' });  // wait for no more than 2 network connections for 500ms    
-                } catch (err) {
-                    throw new PageContactError("page ".concat(urls[i]).concat(" either doesn't exist or there's a network connection error"));
-                }
+        var firstChar = split[1][0];
+        // if link is a valid address...
+        if (!used_urls.has(urls[i]) && firstChar >= '0' && firstChar <= '9') {
+            console.log(urls[i]);
+            console.log(i)
+            used_urls.add(urls[i]);
 
-
-                try {
-                    await page.goto(urls[i], { waitUntil: 'networkidle2' });  // wait for no more than 2 network connections for 500ms    
-                } catch (err) {
-                    throw new PageContactError("page ".concat(urls[i]).concat(" either doesn't exist or there's a network connection error"));
-                }
-
-                // await wait(150000);
-                // async function wait(ms) {
-                //     return new Promise(resolve => {
-                //         setTimeout(resolve, ms);
-                //     });
-                // }
-
-                let pagedata = await page.evaluate(() => {
-                    try {
-                        // Set of data needed from Yahoo
-                        const columns = new Set();
-                        columns.add('Market Cap');
-                        columns.add('Current Ratio');
-                        columns.add('P/E');
-                        columns.add('BV');
-                        columns.add('LTD');
-                        columns.add('NCA');
-
-                        // get all the spans
-                        let spans = document.querySelectorAll('span[class="priv"]');
-
-                        // if these conditions are true, page is not a SOLD property 
-                        if (spans[6] == undefined || spans[6].innerText.localeCompare("Sold") != 0) {
-                            return -3;
-                        }
-
-
-                        let col_names = document.querySelectorAll('dt[class="column-label"]');
-
-                        // skip Walk Score cause this is being weird
-                        let temp = [];
-                        for (var tt = 0; tt < col_names.length; tt++) {
-                            if (col_names[tt].innerText.localeCompare('Walk Score') != 0) {
-                                temp.push(col_names[tt]);
-                            }
-                        }
-                        col_names = temp;
-
-                        // all of these are the same for each page
-                        let address = document.querySelector('section>h1').innerText;
-                        let bedNum = spans[0].innerText;
-                        let bathNum = spans[1].innerText;
-                        let sqarefootage = spans[2].innerText;
-                        let sold_price = spans[3].innerText;
-                        let soldOn = spans[5].innerText;
-
-
-                        // init return
-                        obj =
-                        {
-                            'sold_price': sold_price,
-                            'address': address,
-                            'bedNum': bedNum,
-                            'bathNum': bathNum,
-                            'sqarefootage': sqarefootage,
-                            'soldOn': soldOn,
-                            'Type': null,
-                            'Style': null,
-                            'Size': null,
-                            'Lot Size': null,
-                            'Age': null,
-                            'Taxes': null,
-                            'Walk Score': null,
-                            'Days on Site': null,
-                            'Pets': null,
-                            'Maintenance Fees': null,
-                            'Lease Term': null,
-                            'Possession': null,
-                            'All Inclusive': null
-                        }
-
-
-                        //  PROCESSING AMENITIES
-                        var y = 0;
-                        for (var w = 0; w < col_names.length; w++) {
-                            var col = col_names[w + y].innerText;
-                            // some weird column name has come up
-                            if (!possibleColumns.has(col)) {
-                                // invalid 
-                                return -2;
-                            }
-                            obj[col] = spans[7 + w].innerText;
-                        }
-
-                        // this is the list of nearby amenities
-                        const nearbyCols = [];
-                        nearbyCols.push("Groceries");
-                        nearbyCols.push("Liquor Store");
-                        nearbyCols.push("Restaurants");
-                        nearbyCols.push("Coffee");
-                        nearbyCols.push("Bank");
-                        nearbyCols.push("Gas Station");
-                        nearbyCols.push('Health & Fitness');
-                        nearbyCols.push("Park");
-                        nearbyCols.push("Library");
-                        nearbyCols.push("Medical Care");
-                        nearbyCols.push("Pharmacy");
-                        nearbyCols.push("Mall");
-                        nearbyCols.push("Movie Theatre");
-                        nearbyCols.push("Bar");
-
-                        // get all divs
-                        let divs = document.querySelectorAll('div[class="media-heading"]');
-
-                        //get all distances 
-                        let dists = document.querySelectorAll('span[class="badge badge--small badge--mono"]')
-
-                        // temp object
-                        let ndata = {};
-                        let arr = [];
-                        let lastCol = "";
-                        for (var e = 0; e < divs.length; e++) {
-                            let category = false;
-
-                            for (var r = 0; r < nearbyCols.length; r++) {
-                                let col = nearbyCols[r];
-                                if (divs[e].innerText.includes(col)
-                                    && divs[e].innerText[divs[e].innerText.length - 2] == 'k'
-                                    && divs[e].innerText[divs[e].innerText.length - 1] == 'm'
-                                ) {
-                                    category = true;
-                                    if (arr.length > 0) {
-                                        ndata[lastCol] = arr;
-                                        arr = [];
-                                    }
-                                    lastCol = col;
-                                    break;
-                                } 1
-                            }
-                            if (!category) {
-                                // let o = {};
-                                // o.name = divs[e].innerText
-                                // o.name = dists[e].innerText;
-                                // arr.push(o);
-
-                                // get only the distance i.e. "0.12 km" from this span 
-                                let string = dists[e].innerText
-                                let startind = -1;
-                                let endind = -1;
-                                for (var c = 0; c < string.length; c++) {
-                                    let char = string[c];
-                                    if (char >= "0" && char <= "9" && startind == -1) {
-                                        startind = c;
-                                    } else if (char == 'k') {
-                                        endind = c + 2;
-                                    }
-                                }
-                                string = string.substring(startind, endind);
-                                arr.push(string);
-                            }
-                        }
-
-                        delete ndata[""]
-                        obj['Amenities'] = ndata;
-
-                        let schoolRatings = document.querySelectorAll('div[class="media-text"]');
-                        let allSchoolType = document.querySelectorAll('div[class="media-sub-heading text-secondary"]');
-                        let allSchoolDis = document.querySelectorAll('span[class="badge badge--small badge--mono"]');
-
-                        obj['Nearest Schools'] = []
-                        for (var e = 0; e < schoolRatings.length; e++) {
-                            let temp = {};
-                            temp['type'] = allSchoolType[allSchoolType.length - schoolRatings.length + e].innerText;
-                            temp['distance'] = allSchoolDis[allSchoolDis.length - schoolRatings.length + e].innerText;
-                            temp['rating'] = schoolRatings[e].innerText
-                            obj['Nearest Schools'].push(temp)
-                        }
-
-                        return obj;
-                    } catch (err) {
-                        return -1
-                    }
-                });
-                if (pagedata == -1 || pagedata == -2 || pagedata - 3) {
-                    console.log(pagedata)
-                    continue;
-                }
-
-                // // replace empty spaces in the address with regex and using this as file name
-                // const filename = pagedata.address.replace(/\s/g, '');
-                // const filepath = ("data/".concat(filename)).concat(".json");
-
-
-                // process string
-                // if string contains unit number, only take address
-                let str = pagedata['address'];
-                var j = 0;
-                for (; j < str.length; j++) {
-                    if (str[j] == '-') {
-                        break;
-                    }
-                }
-                if (j != str.length) {
-                    str = str.substring(j + 1);
-                }
-
-                pagedata['address'] = str;
-
-                // get coords from address
-                let coords = await tools.coords(pagedata['address']);
-
-                console.log(pagedata['address'])
-                console.log(coords)
-
-
-                if ('error' in coords ||
-                    Math.abs(coords['lat'] - TOlat) > 5 ||
-                    Math.abs(coords['lon'] - TOlong) > 5
-                ) {
-                    console.log("geodata error");
-                    throw new BadRowError("geodata error");
-                }
-
-                pagedata['longitude'] = coords['lon'];
-                pagedata['latitude'] = coords['lat'];
-
-                pagedata['pageNum'] = pageNum;
-                pagedata['iterationNum'] = i;
-                pagedata['weblink'] = urls[i]
-                console.log(pagedata);
-
-
-                // convert json to string
-                pagedata = JSON.stringify(pagedata);
-
-                // save json object to data folder
-
-                csvCols.push(pagedata);
-
-                if (csvCols.length >= MAXCOLS) {
-
-                    let string = "";
-                    for (var z = 0; z < csvCols.length; z++) {
-                        string += csvCols[z] + '\n';
-                    }
-
-                    fs.appendFile('../Data/data.txt', string, function (err) {
-                        if (err) throw err;
-                        console.log('Saved!');
-                    });
-                    csvCols = [];
-                }
-
+            try {
+                await page.goto(urls[i], { waitUntil: 'networkidle2' });  // wait for no more than 2 network connections for 500ms    
+            } catch (err) {
+                throw new PageContactError("page ".concat(urls[i]).concat(" either doesn't exist or there's a network connection error"));
             }
+
+
+            try {
+                await page.goto(urls[i], { waitUntil: 'networkidle2' });  // wait for no more than 2 network connections for 500ms    
+            } catch (err) {
+                throw new PageContactError("page ".concat(urls[i]).concat(" either doesn't exist or there's a network connection error"));
+            }
+
+            // await wait(150000);
+            // async function wait(ms) {
+            //     return new Promise(resolve => {
+            //         setTimeout(resolve, ms);
+            //     });
+            // }
+
+            let pagedata = await page.evaluate(() => {
+                try {
+                    // Set of data needed from Yahoo
+                    const columns = new Set();
+                    columns.add('Market_Cap');
+                    columns.add('Current_Ratio');
+                    columns.add('P/E');
+                    columns.add('BV'); //Book value
+                    columns.add('LTD'); //Long term dept
+                    columns.add('NCA'); //Net current assets
+
+                    // get all the spans
+                    let spans = document.querySelectorAll('span[class="priv"]');
+
+                    let col_names = document.querySelectorAll('dt[class="column-label"]');
+
+                    // Both on summary page
+                    let mrktCap = document.querySelector('section>h1').innerText; // data-reactid="85"
+                    let pe = spans[1].innerText; // data-reactid="95"
+                    // Rest are on balance sheet
+                    page.goto(yahoo.concat(tick.toString(10)).concat("/balance-sheet?p=").concat(tick.toString(10)));
+                    let currRatio = spans[0].innerText;
+                    let bv = spans[2].innerText;
+                    let ltd = spans[3].innerText;
+                    let nca = spans[5].innerText; //Curr assets - Total Liabilities - Preferred Shares
+
+                    // init return
+                    obj =
+                    {
+                        'Market_Cap': mrktCap,
+                        'Current_Ratio': currRatio,
+                        'P/E': pe,
+                        'BV': bv,
+                        'LTD': ltd,
+                        'NCA': nca
+                    }
+
+                    return obj;
+                } catch (err) {
+                    return -1
+                }
+            });
+            if (pagedata == -1) {
+                console.log(pagedata)
+                continue;
+            }
+
+
+            console.log(pagedata['address'])
+
+
+            pagedata['pageNum'] = pageNum;
+            pagedata['iterationNum'] = i;
+            pagedata['weblink'] = urls[i]
+            console.log(pagedata);
+
+
+            // convert json to string
+            pagedata = JSON.stringify(pagedata);
+
+            // save json object to data folder
+
+            csvCols.push(pagedata);
+
+            if (csvCols.length >= MAXCOLS) {
+
+                let string = "";
+                for (var z = 0; z < csvCols.length; z++) string += csvCols[z] + '\n';
+
+                fs.appendFile('../Data/data.txt', string, function (err) {
+                    if (err) throw err;
+                    console.log('Saved!');
+                });
+                csvCols = [];
+            }
+
         }
 
         try {
